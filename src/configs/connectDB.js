61228512bdb53bf89b6b58f3470bf2c46e04c08a
Nguyen Huu Quyen <nguyenhuuquyen2001@gmail.com>
2022-09-19 22:26:39 +0700

    router params & users details page

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,11 +1,14 @@
 // get the client
-import mysql from 'mysql2'
+import mysql from 'mysql2/promise'
 
 // create the connection to database
-const connection = mysql.createConnection({
+
+console.log('Creating connection pool...')
+
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'nodejsbasic'
 });
 
-export default connection
+export default pool
