b19d4a154f83040f1c2b86478dd3f297e082dc77
Nguyen Huu Quyen <nguyenhuuquyen2001@gmail.com>
2022-09-20 10:56:07 +0700

    using form & post request with nodejs

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -7,11 +7,17 @@ let getHomepage = async (req, res) => {
 
 let getDetailPage = async (req, res) => {
   let userId = req.params.id
-  let [user] = await pool.execute(`select * from users where id = ${userId}`)
-  console.log('>>> check req params: ', user)
+  let [user] = await pool.execute(`select * from users where id = ?`, [userId])
   return res.send(JSON.stringify(user))
 }
 
+let createNewUser = async (req, res) => {
+  const { firstName, lastName, email, address } = req.body
+
+  await pool.execute('insert into users (firstName, lastName, email, address) values (?, ?, ?, ?)', [firstName, lastName, email, address])
+  return res.redirect('/')
+}
+
 module.exports = {
-  getHomepage, getDetailPage
+  getHomepage, getDetailPage, createNewUser
 }
